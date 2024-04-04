e63bc224d9556ee52789a27275ee85cfcf3475d1
Ben McDaniel <ben.mcdaniel370@gmail.com>
2024-04-04 16:25:07 -0400

    change

--- moving_users_pythonversion.py
+++ moving_users_pythonversion.py
@@ -1,22 +1,9 @@
 from subprocess import *
 import time
      
-'''PRACTICE CODE
-
-    for i in range(0,20):
-        if(i == 2):
-            print("value = " + str(i))
-            continue;
-        print(i)
-        
-    print("\nNumber ends at " + str(i))
-
-    print("\nProgram Ends!");
-
-
-    with open('test.txt', 'w') as f:
-    p1=run("ls -l", stdout=f, text=True)
-
+'''NOTES FOR CODE
+shell=True --allows you to pass in additional arguements within your commands.
+check=True -- returns a non-zero error code exit status. If this is not true, the exception will be thrown.
 '''
     
 
@@ -42,26 +29,34 @@ def program():
 def creating_users_permissions(username, password):
     
     print(f"Creating permissions for {username}")
+    try:
+        run(["useradd", "-m", "-s", "/bin/bash", username], check=True)
+        run(["chpasswd"], input=f"{username}:{password}", check=True, shell=True)
+        run(["usermod", "-aG", "sftp", username], check=True)
+        
+        #Disabling ssh for the users
+        
+        run(["sudo", "usermod", "--shell", "/sbin/nologin", username], check=True)
+        
+        #Set drectory permissions for the user
+        
+        run(["sudo", "mkdir", "-p", f"/sftp/chroot/{username}/data"], check=True)
+        run(["sudo", "chown", "root:sftp", f"/sftp/chroot/{username}"], check=True)
+        run(["sudo", "chown" f"{username}:sftp /sftp/chroot/{username}/data"], check=True)
+        run(["sudo", "chmod 750", f"/sftp/chroot/{username}"], check=True)
+        run(["sudo", "chmod 770", f"/sftp/chroot/{username}/data"], check=True)
+        
+        run(["sudo", "mkdir", "-p", f"/sftp/sftpHold/{username}/data"], check=True)
+        run(["sudo", "chown", "root:sftp", f"/sftp/sftpHold/{username}/data"], check=True)
+        run(["sudo", "chmod", "750" f"/sftp/sftpHold/{username}"], check=True)
+        run(["sudo", "chmod", "770", f"/sftp/sftpHold/{username}/data"] check=True)
+        
+        print(f"User {username} has been created and added to the 'sftp' group with directory permissions set.")
+        return True
     
-    run(f"useradd -m -s /bin/bash {username}")
-    run(f"{username}:{password} | chpasswd")
-    run(f"usermod -aG sftp {username}")
-    
-    run(f"sudo usermod --shell /sbin/nologin {username}")
-    #Set drectory permissions for the user
-    run(f"sudo mkdir -p /sftp/chroot/{username}/data")
-    run(f"sudo chown root:sftp /sftp/chroot/{username}")
-    run(f"sudo chown {username}:sftp /sftp/chroot/{username}/data")
-    run(f"sudo chmod 750 /sftp/chroot/{username}")
-    run(f"sudo chmod 770 /sftp/chroot/{username}/data")
-    
-    run(f"sudo mkdir -p /sftp/sftpHold/{username}/data")
-    run(f"sudo chown root:sftp /sftp/sftpHold/{username}/data")
-    run(f"sudo chmod 750 /sftp/sftpHold/{username}")
-    run(f"sudo chmod 770 /sftp/sftpHold/{username}/data")
-    
-    print(f"User {username} has been created and added to the 'sftp' group with directory permissions set.")
-
+    except CalledProcessError as e:
+        print(f"Error wile creating user: {e}")
+        return False
+        
 # Running the program
-
 program()
