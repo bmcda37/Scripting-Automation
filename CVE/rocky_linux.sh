253c2abafbcbef4af2a1b7cff19662730825f0fd
Ben McDaniel <157663194+bmcda37@users.noreply.github.com>
2024-05-21 17:51:53 +0000

    Final Script

--- CVE/rocky_linux.sh
+++ CVE/rocky_linux.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#! /usr/bin/env bash
 
 CONFIG_FILE="/etc/dnf/automatic.conf"
 UPGRADE_TYPE="security"
@@ -8,10 +8,14 @@ sudo dnf install -y dnf-automatic
 
 sed -i "s/upgrade_type = .*/upgrade_type = $UPGRADE_TYPE/" $CONFIG_FILE
 
-sed -i "s/random_sleep = ."/random_sleep = $SLEEP/" $CONFIG_FILE
+sed -i "s/random_sleep = .*/random_sleep = $SLEEP/" $CONFIG_FILE
 
 systemctl enable --now dnf-automatic.timer
 systemctl start dnf-automatic.timer
 
-echo systemctl status dnf-automatic.timer
-echo systemctl list-timers dnf-automatic.timer
+status=$(systemctl status dnf-automatic.timer)
+
+current_timers=$(systemctl list-timers dnf-automatic.timer)
+
+echo "$status"
+echo "$current_timers"
