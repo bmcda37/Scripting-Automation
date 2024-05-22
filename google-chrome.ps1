622b50749fbafb94e50bca5f91715c3d2e5180e8
Ben McDaniel <ben.mcdaniel370@gmail.com>
2024-05-22 10:35:56 -0400

    final

--- google-chrome.ps1
+++ google-chrome.ps1
@@ -10,16 +10,18 @@ function IsProcessRunning {
 }
 
 # Path to Google Update executable
-$GoogleUpdatePath = "C:Program Files (x86)\Google\Update\GoogleUpdate.exe"
+$GoogleUpdatePath = "C:\Program Files (x86)\Google\Update\GoogleUpdate.exe"
 
-$ChromePath = "C:Program Files\Google\Chrome\Application\chrome.exe"
+$ChromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
 $ChromeRunning = IsProcessRunning "chrome"
 
 # If Chrome is running, close it
 if ($ChromeRunning) {
     Write-Output "Google Chrome is running. Closing it..."
+    Start-Sleep -Seconds 1
     Stop-Process -Name "chrome" -Force
+    Start-Sleep -Seconds 1
     Write-Output "Google Chrome closed."
 }
 
@@ -27,19 +29,23 @@ if ($ChromeRunning) {
 # Check if the Google Update executable exists
 if (Test-Path $GoogleUpdatePath) {
     Write-Output "Google Update executable found. Initiating update check..."
-
+    Start-Sleep -Seconds 1
     # Start the Google Update process to check for updates and apply them
     Start-Process -FilePath $GoogleUpdatePath -ArgumentList "/ua" -Wait
 
     Write-Output "Google Chrome update check initiated."
+    Start-Sleep -Seconds 1
 } else {
     Write-Output "Google Update executable not found. Ensure Google Chrome is installed correctly."
+    Start-Sleep -Seconds 1
 }
 
 
 # If Chrome was running before, relaunch it
 if ($ChromeRunning) {
     Write-Output "Relaunching Google Chrome..."
+    Start-Sleep -Seconds 1
     Start-Process -FilePath $ChromePath
     Write-Output "Google Chrome relaunched."
+    Start-Sleep -Seconds 1
 }
---
+++ scheduled-task.ps1
@@ -0,0 +1,5 @@
+
+$trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Sunday,Wednesday -At 5:00am
+$action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument '-NoProfile -ExecutionPolicy Bypass -File "C:\scripts\google-chrome.ps1"'
+
+Register-ScheduledTask -TaskName "GoogleRefresh" -TaskPath 'GoogleRefresh' -Action $action -Trigger $trigger 
