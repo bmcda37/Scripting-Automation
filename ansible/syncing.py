8c58c873776a6ce369a5ed624fc35cf0e724ae54
Ben McDaniel <ben.mcdaniel370@gmail.com>
2024-08-06 14:56:01 -0400

    commit

--- ansible/syncing.py
+++ ansible/syncing.py
@@ -1,51 +1,71 @@
+#!
+
 import configparser
 import os
-import glob
 import subprocess
-import time
 import shutil
-
-
-# Read the config file
-config = configparser.ConfigParser()
-config.sections()
 
 #Create a folder for any new groups in host
 def create_folder():
-       
-    for section in config.sections():
-            src_dir = './'
+    config = configparser.ConfigParser()
+      
+    with open('host.ini', 'r') as f:
+        config.read_file(f)
+    
+        for section in config.sections():
+                
+            src_dir = './general'
             dst_dir = f'./{section}'
-
+                
+            #If the group does not have a directory make the directory
             if not os.path.exists(dst_dir):
-                print(f'Creating folder: {dst_dir}')
-                os.makedirs(dst_dir)
+                try:
+                    print(f'Creating folder: {dst_dir}')
+                    os.makedirs(dst_dir)
+                except OSError as o:
+                    print(f'Error making directory for {section}: {o}')    
+                
+            #If the group does have a directory
+            if os.path.exists(dst_dir):
+                
+                #Lists all the files in my general directory
+                files = os.listdir(src_dir)
 
-            files = os.listdir(src_dir)
+                #Get the full path with file name
+                for file_name in files:
+                    src_addr = os.path.join(src_dir, file_name)
+                    dst_addr = os.path.join(dst_dir, file_name)
 
-            for file_name in files:
-                src_addr = os.path.join(src_dir, file_name)
-                dst_addr = os.path.join(dst_dir, file_name)
+                #If there is no file in my src directory
+                    if os.path.isfile(src_addr) and not os.path.isfile(dst_addr):
+                        print(f'File exists in {src_addr}. Copying the files...')
+                    
+                        try:
+                        # Copy the folder recursively to the new location
+                            shutil.copy2(src_addr, dst_addr)
+                            print(file_name)
+                            # Replace values in the copied file
+                            switch("<group>",section, dst_addr)   
+                        except IOError as e:
+                            print(f'Errory copying file {file_name} to {dst_addr}: {e}')
+                    else:
+                    #Search the files that exists and change any <group> to section name.
+                        switch("<group>",section, dst_addr)
 
-                if os.path.isfile(src_addr):
-                    # Copy the folder recursively to the new location
-                    shutil.copytree(src_addr, dst_addr)
-                    # Replace values in the copied file
-                    for file in dst_addr:
-                        with open(file, 'r') as f:
-                            filedata = f.read()
-                            subprocess.run(["sed", "-i", f"s/<group>/{section}/g", file])
-                else:
-                    print(f'{src_addr} is not a file.')
-  
-  
-
-#def sync_files(src,dst):
-#    command = ["rsync", "-av", src, dst]
-#    subprocess.run([command], capture_output=True)
+def switch(old, new, file_path):
+    #file_path = "./general/test.txt"
+    
+    # Read the file's content
+    with open(file_path, "r") as f:
+        file_data = f.read()
+    
+    # Replace <group> with the source value
+    file_data = file_data.replace(old, new)
+    
+    # Write the updated content back to the file
+    with open(file_path, "w") as f:
+        f.write(file_data)
 
 
 if __name__ == "__main__":
     create_folder()
-    #src = "./"
-    #dst = "../"
